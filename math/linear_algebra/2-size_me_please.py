db0a8bd699c268a06a85958c832d557d22af7576
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-03 12:30:35 +0400

    I think everything is okay

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -18,11 +18,11 @@ def matrix_shape(matrix):
         shape.append(row)
         shape.append(column)
         return shape
-    if isinstance(matrix[0][0], list) and isinstance(matrix[0][0][0], int):
+    elif isinstance(matrix[0][0], list) and isinstance(matrix[0][0][0], int):
         row = len(matrix)
         column = len(matrix[0])
-        count = len(matrix[0][0])
+        slices = len(matrix[0][0])
         shape.append(row)
         shape.append(column)
-        shape.append(count)
+        shape.append(slices)
         return shape
