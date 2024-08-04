8dc2fa6c902b6129be307fc329f38eb06a9e72c4
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 17:03:48 +0400

    smth fix

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -16,21 +16,28 @@ def add_matrices2D(mat1, mat2):
     j = 0
     if len(m1) == 0 and len(m2) == 0:
         return [[], []]
+    if len(m1[0]) == 0 and len(m2[0] == 0):
+        return [[], []]
     if len(m1[0]) > 0 and len(m2[0]) > 0:
-        if isinstance(m1[0], list) and isinstance(m2[0], list):
-            if isinstance(m1[0][0], int) or isinstance(m1[0][0], float):
-                if isinstance(m2[0][0], int) or isinstance(m2[0][0], float):
-                    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
-                        for i in range(len(m1)):
-                            for j in range(len(m1[0])):
-                                sum = m1[i][j] + m2[i][j]
-                                empty.append(sum)
-                                j += 1
-                            new_arr.append(empty)
-                            empty = []
-                            i += 1
-                        return new_arr
-                    else:
-                        return None
+        if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+            for i in range(len(m1)):
+                for j in range(len(m1[0])):
+                    sum = m1[i][j] + m2[i][j]
+                    empty.append(sum)
+                    j += 1
+                new_arr.append(empty)
+                empty = []
+                i += 1
+            return new_arr
+        else:
+            return None
     else:
         return [[], []]
+
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
