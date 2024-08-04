8e90b08567dc7384596fb669b7591a07d87b911b
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 16:49:32 +0400

    correcting all conditions

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -11,5 +11,14 @@ def cat_arrays(arr1, arr2):
     new_arr = []
     if isinstance(arr1[0], int) or isinstance(arr1[0], float):
         if isinstance(arr2[0], int) or isinstance(arr2[0], float):
-            new_arr = arr1 + arr2
-            return new_arr
+            if len(arr1) > 0 and len(arr2) > 0:
+                new_arr = arr1 + arr2
+                return new_arr
+            if len(arr1) == 0 and len(arr2) == 0:
+                return []
+            if len(arr1) > 0 and len(arr2) == 0:
+                new_arr = arr1
+                return new_arr
+            elif len(arr1) == 0 and len(arr2) > 0:
+                new_arr = arr2
+                return new_arr
