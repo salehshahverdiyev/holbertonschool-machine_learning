67eb613c581a247acd5d25d0b8c78b7f5c47ab60
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 16:55:42 +0400

    correcting values

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -15,8 +15,8 @@ def cat_arrays(arr1, arr2):
     if len(arr1) == 0 and len(arr2) == 0:
         return []
     if len(arr1) > 0 and len(arr2) == 0:
-        new_arr = arr1
+        new_arr += arr1
         return new_arr
-    elif len(arr1) == 0 and len(arr2) > 0:
-        new_arr = arr2
+    if len(arr1) == 0 and len(arr2) > 0:
+        new_arr += arr2
         return new_arr
