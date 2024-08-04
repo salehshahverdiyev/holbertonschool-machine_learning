81fb7c44f500e17bdc6fd94791e579d9afbb34d3
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 20:24:37 +0400

    Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''
+        Function Documentation
+    '''
+    return np.dot(mat1, mat2)
