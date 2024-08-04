334482fa3e6ac74369c248b8c465c80035ccc25f
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 10:57:26 +0400

    Line Up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+
+
+def add_arrays(arr1, arr2):
+    '''
+        Function Documentation
+    '''
+    new_arr = []
+    if isinstance(arr1[0], int) or isinstance(arr1[0], float):
+        if isinstance(arr2[0], int) or isinstance(arr2[0], float):
+            if len(arr1) == len(arr2):
+                for i in range(len(arr1)):
+                    sum = arr1[i] + arr2[i]
+                    new_arr.append(sum)
+                    i += 1
+                return new_arr
+            elif len(arr1) != len(arr2):
+                return None
