d25404b94c2f9565550150dbf572bfd5915defb2
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 21:39:33 +0400

    different approach and structure

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,17 +5,22 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    '''
-        Function Documentation
-    '''
-    new_mat = []
-    i = 0
-    if len(mat1) != len(mat2):
-        new_mat = mat1 + mat2
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        new_mat = []
+        for row in mat1:
+            new_mat.append(row)
+        for row in mat2:
+            new_mat.append(row)
         return new_mat
-    if axis == 1 and len(mat1) == len(mat2):
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        new_mat = []
         for i in range(len(mat1)):
-            temp = mat1[i] + mat2[i]
-            new_mat.append(temp)
-            i += 1
+            new_row = mat1[i] + mat2[i]
+            new_mat.append(new_row)
         return new_mat
+    else:
+        return None
