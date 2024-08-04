b7636d4e23552848d59b50f9f9981e6e284183b8
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-04 11:13:18 +0400

    try to fix vol3

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -9,9 +9,9 @@ def add_arrays(arr1, arr2):
         Function Documentation
     '''
     new_arr = []
-    if isinstance(arr1[0], int) or isinstance(arr1[0], float):
-        if isinstance(arr2[0], int) or isinstance(arr2[0], float):
-            if len(arr1) > 0 and len(arr2) > 0:
+    if len(arr1) > 0 and len(arr2) > 0:
+        if isinstance(arr1[0], int) or isinstance(arr1[0], float):
+            if isinstance(arr2[0], int) or isinstance(arr2[0], float):
                 if len(arr1) == len(arr2):
                     for i in range(len(arr1)):
                         sum = arr1[i] + arr2[i]
@@ -20,5 +20,5 @@ def add_arrays(arr1, arr2):
                     return new_arr
                 elif len(arr1) != len(arr2):
                     return None
-            else:
-                return None
+    else:
+        return None
