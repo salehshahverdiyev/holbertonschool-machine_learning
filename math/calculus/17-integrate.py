8708eba8e86c7e13cfab5b9137408b96b10b9856
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-11 21:37:33 +0400

    Integrate

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+
+
+def poly_integral(poly, C=0):
+    '''
+        Function Documentation
+    '''
+    if not isinstance(poly, list):
+        return None
+    for c in poly:
+        if not isinstance(c, (int, float)):
+            return None
+    if not isinstance(C, (int, float)):
+        return None
+    integral = [C]
+    for i in range(len(poly)):
+        coeff = poly[i]
+        integral_coeff = coeff / (i + 1)
+        if integral_coeff.is_integer():
+            integral.append(int(integral_coeff))
+        else:
+            integral.append(integral_coeff)
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
+    return integral
