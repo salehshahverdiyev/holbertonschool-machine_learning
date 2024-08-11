99e2a229c754e4fa64e3e6843f3f2c6ac53a49cc
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-11 15:58:46 +0400

    color fixing, we need default color

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -15,7 +15,7 @@ def change_scale():
     t = 5730
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, 'b-')
+    plt.plot(x, y)
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of C-14')
