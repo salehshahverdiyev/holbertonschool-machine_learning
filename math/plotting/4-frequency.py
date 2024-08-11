2230edbbc151a6632c6ff1e83acae4a43f5998ef
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-11 14:45:38 +0400

    Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    '''
+        Function Documentation
+    '''
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
