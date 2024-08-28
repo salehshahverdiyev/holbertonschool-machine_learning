64afb3481fd7370c1325f9191bc3254234bc5352
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-08-28 22:39:25 +0400

    class constructor

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -8,18 +8,6 @@ import numpy as np
 
 class NeuralNetwork:
     '''
-        class constructor: def __init__(self, nx, nodes):
-            nx is the number of input features
-                If nx is not an integer, raise a TypeError
-                    with the exception: nx must be an integer
-                If nx is less than 1, raise a ValueError
-                    with the exception: nx must be a positive integer
-            nodes is the number of nodes found in the hidden layer
-                If nodes is not an integer, raise a TypeError
-                    with the exception: nodes must be an integer
-                If nodes is less than 1, raise a ValueError
-                    with the exception: nodes must be a positive integer
-            All exceptions should be raised in the order listed above
         Public instance attributes:
             W1: The weights vector for the hidden layer. Upon instantiation,
                 it should be initialized using a random normal distribution.
@@ -34,17 +22,32 @@ class NeuralNetwork:
             A2: The activated output for the output neuron (prediction).
                 Upon instantiation, it should be initialized to 0.
     '''
-    if isinstance(nx, int) is False:
-        raise TypeError("nx must be an integer")
-    if nx < 1:
-        raise ValueError("nx must be a positive integer")
-    if isinstance(nodes, int) is False:
-        raise TypeError("nodes must be an integer")
-    if nodes < 1:
-        raise ValueError("nodes must be a positive integer")
-    self.W1 = np.random.normal(0, 1, (nodes, nx))
-    self.b1 = np.zeros((nodes, 1))
-    self.A1 = 0
-    self.W2 = np.random.normal(0, 1, (1, nodes))
-    self.b2 = 0
-    self.A2 = 0
+    def __init__(self, nx, nodes):
+        '''
+            class constructor: def __init__(self, nx, nodes):
+                nx is the number of input features
+                    If nx is not an integer, raise a TypeError
+                        with the exception: nx must be an integer
+                    If nx is less than 1, raise a ValueError
+                        with the exception: nx must be a positive integer
+                nodes is the number of nodes found in the hidden layer
+                    If nodes is not an integer, raise a TypeError
+                        with the exception: nodes must be an integer
+                    If nodes is less than 1, raise a ValueError
+                        with the exception: nodes must be a positive integer
+                All exceptions should be raised in the order listed above
+        '''
+        if isinstance(nx, int) is False:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if isinstance(nodes, int) is False:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(0, 1, (nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(0, 1, (1, nodes))
+        self.b2 = 0
+        self.A2 = 0
