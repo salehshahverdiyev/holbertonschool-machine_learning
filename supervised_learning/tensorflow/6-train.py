fd04d31667b08c778ba84dbbe6cebee84baf3a83
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-09-08 16:25:21 +0400

    try to fix, but I think everything is correct

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -4,11 +4,11 @@
 '''
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
