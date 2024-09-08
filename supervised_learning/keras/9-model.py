11df043431a2e2f46a6aab894cae07f542eeddf9
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-09-08 20:21:59 +0400

    Save and Load Model

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    '''
+        saves an entire model:
+    network is the model to save
+    filename is the path of the file
+        that the model should be saved to
+    Returns: None
+    '''
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    '''
+         loads an entire model:
+    filename is the path of the file
+        that the model should be loaded from
+    Returns: the loaded model
+    '''
+    return K.models.load_model(filename)
