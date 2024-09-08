8d2d3e27edba8316dd7ffef640c8a715741c9424
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-09-08 20:55:30 +0400

    Predict

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    '''
+        makes a prediction using a neural network:
+        network is the network model to make the prediction with
+        data is the input data to make the prediction with
+        verbose is a boolean that determines if output
+            should be printed during the prediction process
+        Returns: the prediction for the data
+    '''
+    return network.predict(data, verbose=verbose)
