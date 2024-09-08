62d4b968f71d1f49cfae4d3f4c469e7abed1f3d6
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2024-09-08 20:27:20 +0400

    Save and Load Weights

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''
+    Script Documentation
+'''
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='keras'):
+    '''
+        saves a model's weights:
+        network is the model whose weights should be saved
+        filename is the path of the file
+            that the weights should be saved to
+        save_format is the format in which
+            the weights should be saved
+        Returns: None
+    '''
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    '''
+        loads a model's weights:
+        network is the model to which
+            the weights should be loaded
+        filename is the path of the file
+            that the weights should be loaded from
+        Returns: None
+    '''
+    network.load_weights(filename)
+    return None
